cc8ac57bafb415a31e9bda6e4f513aea80c1ea9a
anushag1104@gmail.com <anushag1104@gmail.com>
2020-03-17 15:36:07 +0530

    Add project files.

---
+++ LoginMVC/Controllers/LoginController.cs
@@ -0,0 +1,44 @@
+﻿using LoginMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace LoginMVC.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Check(Login L)
+        {
+            if (ModelState.IsValid)
+            {
+                bool b=DBOperations.Login(L);
+                if (b)
+                    return RedirectToAction("Home");
+                else
+                {
+                    ViewBag.msg = "Invalid Credentials";
+                    return View("Index");
+                }
+            }
+               
+            else
+                return View("Index");
+        }
+
+        public ActionResult Home()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ LoginMVC/Models/DBOperations.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LoginMVC.Models
+{
+    public class DBOperations
+    {
+        static ADODBEntities1 a = new ADODBEntities1();
+
+        public static bool Login(Login L)
+        {
+            
+            var l = from i in a.Logins
+                    where i.username == L.username && i.password == L.password
+                    select i;
+            if (l.Count() == 1)
+                return true;
+            else 
+                return false;
+        }
+    }
+}
